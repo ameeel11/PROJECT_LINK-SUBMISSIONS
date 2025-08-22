f247e9b2df1a4084a5125f3639158e0a23216db9
amelll <ameliafibrianti33@gmail.com>
2025-08-22 20:06:41 +0700

    Update script.js

--- Development/Frontend/Newcomer/Amel-js-interaction/script.js
+++ Development/Frontend/Newcomer/Amel-js-interaction/script.js
@@ -1,1 +1,6 @@
-
+document.getElementById("changeColorBtn").onclick = function() {
+  const colors = ["#f5f3b8ff", "#e0f7fa", "#b5feb4ff", "#f7c3ffff"];
+  const chosen = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.backgroundColor = chosen;
+  alert("Background diubah!");
+};
